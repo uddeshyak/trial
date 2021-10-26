ef6534d2c06ef23c1685740eb69d79efd07e1f39
uddeshya kumar <uddeshya2012@gmail.com>
2021-10-27 03:13:58 +0530

    UPDATE1

--- class and object.py
+++ class and object.py
@@ -1,3 +1,5 @@
+#CLASS , OBJECT AND INHERITANCE:-
+
 from student import student
 
 student1 = student("raj", 21, 'business')
@@ -6,3 +8,12 @@ student2 =student("nik", 32 ,"law")
 
 print(student1.name)
 print(student1.ranking())
+
+from chef import chef
+
+mychef = chef()
+mychef.make_chicken()
+mychef.make_salad()
+
+
+
